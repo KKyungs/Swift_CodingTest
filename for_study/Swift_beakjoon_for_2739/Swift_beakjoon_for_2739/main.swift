cd2c56948f3a754aca7917a24695f29fcc4d92f5
KKyungs <wnrud675@gmail.com>
2021-08-09 20:00:32 +0900

    day05

---
+++ for_study/Swift_beakjoon_for_2739/Swift_beakjoon_for_2739/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Swift_beakjoon_for_2739
+//
+//  Created by 이주경 on 2021/08/09.
+//
+//  for 문제
+//  구구단
+
+import Foundation
+
+var num = Int(readLine()!)!
+
+for i in 1..<10 {
+  print("\(num) * \(i) = \(num*i)")
+}
