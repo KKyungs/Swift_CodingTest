6f5ea5d3f37a6495d2062409e39ea52dbdcbd204
KKyungs <wnrud675@gmail.com>
2021-08-10 22:34:09 +0900

    day06

---
+++ for_study/Swift_beakjoon_for_8393/Swift_beakjoon_for_8393/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Swift_beakjoon_for_8393
+//
+//  Created by 이주경 on 2021/08/10.
+//
+
+import Foundation
+
+let num = Int(readLine()!)!
+var s = 0
+for i in 1...num {
+    s += i
+}
+print(s)
