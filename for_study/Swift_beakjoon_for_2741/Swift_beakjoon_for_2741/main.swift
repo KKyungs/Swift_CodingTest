d82872d4c0595c1346a67e4a19a6190d1b4598de
KKyungs <wnrud675@gmail.com>
2021-08-12 14:06:28 +0900

    day07

---
+++ for_study/Swift_beakjoon_for_2741/Swift_beakjoon_for_2741/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Swift_beakjoon_for_2741
+//
+//  Created by 이주경 on 2021/08/12.
+//
+//  백준
+//  for 문제
+//  N 찍기
+
+
+import Foundation
+
+let num = Int(readLine()!)!
+
+for i in 1 ... num {
+    print(i)
+}
